491cdd78819b9c4958d803cb6b53475c67606da5
Christian Pinto <pintochristian2@gmail.com>
2019-04-21 01:05:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,6 +13,6 @@ def oxford_comma(array)
     array.insert(size - 1, "and")
      list =  array.join(", ")
      comma = list.index("and")
-     list.insert(comma + 4, "")
+     list.insert(comma + 3, "")
     end
 end
