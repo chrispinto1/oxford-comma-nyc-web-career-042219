5f09703d419faff60558a3fa61803ad6b4e5ef90
Christian Pinto <pintochristian2@gmail.com>
2019-04-21 00:40:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
       i = 0
       string = ""
       while i < array.size
-        string << array.join(",")
+        string << array
         i+=1
       end
       new_string = string + " and " + array.last
