738e4388f03cecb8a39092494869042ba15f730e
Christian Pinto <pintochristian2@gmail.com>
2019-04-21 00:46:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     elsif size == 3
       array.insert(2, "and")
       string = ""
-      string << array.join(" ")
-      
+      string << array.join(", ")
+      string.delete()
     end
 end
